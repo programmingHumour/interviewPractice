cec82e6255bbbc801ec93239441bf09bed5c90ed
Sai Kiran Kotha <sweetypie@Sais-MacBook-Pro.local>
2018-09-22 10:27:15 -0700

    largest contionus sum

--- pythonPractice/LongestContinuousSum.py
+++ pythonPractice/LongestContinuousSum.py
@@ -17,3 +17,27 @@ def largestContinuousSum(arr):
 array = [-1,-2,-1,-3,4,10,10,-10,-1]
 maximumContionousSum = largestContinuousSum(array)
 print(maximumContionousSum)
+
+# def compress(string):
+#
+#     res = ""
+#
+#     count = 1
+#
+#     #Add in first character
+#     res += string[0]
+# 
+#     #Iterate through loop, skipping last one
+#     for i in range(len(string)-1):
+#         if(string[i] == string[i+1]):
+#             count+=1
+#         else:
+#             if(count > 1):
+#                 #Ignore if no repeats
+#                 res += str(count)
+#             res += string[i+1]
+#             count = 1
+#     #print last one
+#     if(count > 1):
+#         res += str(count)
+#     return res
